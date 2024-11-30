7a03486a517dfa127494f710a78dc508694e57a9
YeahOut <pt840072@gmail.com>
2024-11-30 22:04:26 +0900

    feat: 개발 초기 설정

---
+++ Mesh_backend/src/main/java/com/example/mesh_backend/MeshBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mesh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeshBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeshBackendApplication.class, args);
+	}
+
+}
---
+++ Mesh_backend/src/test/java/com/example/mesh_backend/MeshBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mesh_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeshBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
